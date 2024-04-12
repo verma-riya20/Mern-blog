1147ffada8febe34b738279ba29bfb12a1b375c0
verma20 <riya204verma@gmail.com>
2024-04-12 22:11:30 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,14 @@
-import { Console } from 'console';
+import mongoose from 'mongoose';
 import  express  from 'express';
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+mongoose
+.connect(process.env.MONGO).
+then(()=>{console.log('MongoDB is connected')}).catch(err=>{
+    console.log(err);
+})
 
 
 const app=express();
